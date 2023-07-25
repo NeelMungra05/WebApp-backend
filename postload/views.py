3ebe09a31e36ada73378712336c1b90123f03c6d
NeelMungra05 <nmungra@deloitte.com>
2023-07-25 13:48:29 +0530

    Made the file reading part from the request
    -> In Reconciliation/urls.py included all the urls from the
    postload/urls.py
    -> Added url path for the recon in postload/urls.py
    -> Bare minimum function for the multiple file upload reading in
    views.py

--- postload/views.py
+++ postload/views.py
@@ -1,3 +1,19 @@
+import pandas as pd
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class ReconUser(APIView):
+    def post(self, request, format=None):
+        fileName = request.FILES.getlist('source')
+
+        var = {}
+
+        for f in fileName:
+            df = pd.DataFrame(f)
+            var[f.name] = df.head()
+
+        return Response({'data': var})
