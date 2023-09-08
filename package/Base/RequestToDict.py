b4312f3acb5b7881fd985dbfbe5f350577834b44
NeelMungra05 <nmungra@deloitte.com>
2023-09-08 11:48:28 +0530

    Made major changes as follow
    -> Instead of using json dumps, now will take directly the data
    -> The request data can be of 4 instance as of now
    -> Assignded that request data to result

--- package/Base/RequestToDict.py
+++ package/Base/RequestToDict.py
@@ -1,9 +1,31 @@
 import json
 
-from django.http import HttpRequest
+from rest_framework.request import Request
+from django.http import QueryDict
+from typing import Literal, Union
 
 
 class ReqToDict:
-    def __init__(self, request: HttpRequest, attr: str) -> None:
-        str_result: str | None = request.POST.get(attr)
-        self.result: dict = json.loads(str_result if str_result else " ")
+    def __init__(
+        self, request: Request, attr: str, type: Literal["str", "int", "dict", "list"]
+    ) -> None:
+        raw_result: dict = {}
+
+        if isinstance(request.data, dict):
+            raw_result = request.data
+
+        result: Union[str, list, dict, int] = ""
+
+        if type == "list" and isinstance(raw_result.get(attr), list):
+            result = raw_result.get(attr, [])
+
+        if type == "dict" and isinstance(raw_result.get(attr), dict):
+            result = raw_result.get(attr, {})
+
+        if type == "str" and isinstance(raw_result.get(attr), str):
+            result = raw_result.get(attr, "")
+
+        if type == "int" and isinstance(raw_result.get(attr), int):
+            result = raw_result.get(attr, int)
+
+        self.result: int | str | list | dict = result
