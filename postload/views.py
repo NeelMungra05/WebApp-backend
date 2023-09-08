648aa7bc9e840fa93172eefafcba7a8d4dc0683c
NeelMungra05 <nmungra@deloitte.com>
2023-09-08 11:28:28 +0530

    Changed from HttpRequest to rest_framework Request

--- postload/views.py
+++ postload/views.py
@@ -1,7 +1,7 @@
-
 from django.http import HttpRequest
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.request import Request
 
 from package import JoinProcess, ReadFields, ReadFile, Reconciliation
 
@@ -9,21 +9,19 @@ from package import JoinProcess, ReadFields, ReadFile, Reconciliation
 
 
 class ReconUser(APIView):
-    def post(self, request: HttpRequest, format=None) -> Response:
-        sourceFiles = ReadFile(
-            request.FILES.getlist('source'))
+    def post(self, request: Request, format=None) -> Response:
+        sourceFiles = ReadFile(request, "source")
 
-        targetFiles = ReadFile(
-            request.FILES.getlist('target'))
+        targetFiles = ReadFile(request, "target")
 
         sourceFields = ReadFields(request, "sourceFields")
         targetFields = ReadFields(request, "targetFields")
 
-        sourceJoinsProcess = JoinProcess(request, "joins", 'sourceJoins')
+        sourceJoinsProcess = JoinProcess(request, "joins", "sourceJoins")
         sourceJoinsProcess.perform_operation(sourceFiles, sourceFields)
         sourceResult = sourceJoinsProcess.get_join_result()
 
-        targetJoinsProcess = JoinProcess(request, "joins", 'targetJoins')
+        targetJoinsProcess = JoinProcess(request, "joins", "targetJoins")
         targetJoinsProcess.perform_operation(targetFiles, targetFields)
         targetResult = targetJoinsProcess.get_join_result()
 
@@ -32,4 +30,6 @@ class ReconUser(APIView):
 
         kpis = recon.kpis()
 
-        return Response({"source vs target": s_vs_t, "target vs source": t_vs_s, "kpis": kpis})
+        return Response(
+            {"source vs target": s_vs_t, "target vs source": t_vs_s, "kpis": kpis}
+        )
